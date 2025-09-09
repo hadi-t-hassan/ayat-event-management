cb0a10ccdd5b61b40674027223e213d58889083e
hadi <haditalalhassan@gmail.com>
2025-09-09 18:21:16 +0300

    modify api url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,8 +1,10 @@
-const isDevelopment = process.env.NODE_ENV === 'development';
-
-export const API_URL = isDevelopment 
+const API_URL = import.meta.env.DEV 
   ? 'http://localhost:8000/api'
   : 'https://pingtech.pythonanywhere.com/api';
+
+console.log('Current API URL:', API_URL); // For debugging
+
+export { API_URL };
 
 export default {
   API_URL,
